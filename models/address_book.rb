f4698d76e8c8f3eeb18c0804c437e26c7db4dd5b
Matt Cheah <matt.cheah@gmail.com>
2017-03-01 19:47:37 +0000

    add option to import from CSV

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative "entry"
+require "csv"
 
 class AddressBook
     attr_reader :entries
@@ -8,7 +9,7 @@ class AddressBook
         @entries = []
     end
     
-    def add_entry(name, email, phone_number)
+    def add_entry(name, phone_number, email)
         index = 0
         
         entries.each do |entry|
@@ -17,7 +18,28 @@ class AddressBook
             end
             index += 1
         end
-        entries.insert(index, Entry.new(name, email, phone_number))
+        entries.insert(index, Entry.new(name, phone_number, email))
     end
-
+    
+    def remove_entry(name, phone_number, email)
+        index = 0
+        entries.each do |entry|
+            if entry.name == name && entry.email == email && entry.phone_number == phone_number
+                entries.delete_at(index)
+                return
+            end
+            index += 1
+        end
+    end
+    
+    def import_from_csv(file_name)
+        csv_text = File.read(file_name)
+        csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+         
+        csv.each do |row|
+            row_hash = row.to_hash
+            add_entry(row_hash['name'], row_hash['phone_number'], row_hash['email'])
+        end
+    end
+    
 end
